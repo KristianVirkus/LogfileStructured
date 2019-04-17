dc1edf8d5321fbec63bd75a0205ce0ee4c5c1c01
Kristian Virkus <kristian.virkus@gmail.com>
2019-04-17 23:01:59 +0200

    Resolves #2: Extension method implemented. Sample modified.

---
+++ tests/Logfile.Structured.UnitTests/LogfileConfigurationBuilderExtensionsTest.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Logfile.Core;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Logfile.Structured.UnitTests
+{
+	class LogfileConfigurationBuilderExtensionsTest
+	{
+		public class AddStructuredLogfile
+		{
+			[Test]
+			public void LogfileConfigurationBuilderNull_ShouldThrow_ArgumentNullException()
+			{
+				// Arrange
+				// Act
+				// Assert
+				Assert.Throws<ArgumentNullException>(() =>
+					LogfileConfigurationBuilderExtensions.AddStructuredLogfile<StandardLoglevel>(null, (_builder) => { }));
+			}
+
+			[Test]
+			public void CallWithLogfileConfigurationBuilder_ShouldReturn_SameInstance()
+			{
+				// Arrange
+				var logfileConfigurationBuilder = new LogfileConfigurationBuilder<StandardLoglevel>();
+
+				// Act
+				// Assert
+				logfileConfigurationBuilder.AddStructuredLogfile((_builder) => { }).Should().BeSameAs(logfileConfigurationBuilder);
+			}
+
+			[Test]
+			public void WorkOnBuilderInCallback_Should_ReflectConfiguration()
+			{
+				// Arrange
+				var logfileConfigurationBuilder = new LogfileConfigurationBuilder<StandardLoglevel>();
+
+				// Act
+				logfileConfigurationBuilder.AddStructuredLogfile((_builder) =>
+				{
+					_builder.UseAppName("test-app");
+				});
+
+				// Assert
+				var router = logfileConfigurationBuilder.Routers.OfType<Router<StandardLoglevel>>().Single();
+				var configurationField = ((Configuration<StandardLoglevel>)router
+						.GetType()
+						.GetField("configuration", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+						.GetValue(router));
+				configurationField.AppName.Should().Be("test-app");
+			}
+		}
+	}
+}
