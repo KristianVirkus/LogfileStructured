d2146a9a2c1cdcc5600c91faff9f50d09b3f0316
Kristian Virkus <kristian.virkus@gmail.com>
2019-08-16 18:07:57 +0200

    Resolves issue #18: Backticks in output hex/binary dump replaced by non-printable character.

--- src/Logfile.Structured/Formatters/Binary.cs
+++ src/Logfile.Structured/Formatters/Binary.cs
@@ -91,7 +91,7 @@ namespace Logfile.Structured.Formatters
 				IncludeHex,
 				IncludeTranscript,
 				NonPrintableCharacterSubstitute);
-			return $"{Introduction}{Constants.NewLine}{data}";
+			return $"{Introduction}{Constants.NewLine}{data.Replace(Event.QuotationSign, NonPrintableCharacterSubstitute)}";
 		}
 
 		#endregion
